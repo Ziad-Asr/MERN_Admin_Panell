7d5c9ab7d87bb220739c888ed323acd568691d64
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-03-04 22:41:04 +0200

    update

--- Backend/index.js
+++ Backend/index.js
@@ -6,10 +6,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import mongoose from "mongoose";
 
-import clientsRoutes from "./routes/clients";
-import generalRoutes from "./routes/general";
-import managementRoutes from "./routes/management";
-import salesRoutes from "./routes/sales";
+import clientsRoutes from "./routes/clients.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 const app = express();
 
@@ -19,10 +19,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginOpenerPolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginOpenerPolicy({ policy: "same-origin" }));
 app.use(morgan("common"));
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log(`DB is connected `);
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log(`DB is not connected`);
+  });
 
 app.use("/clients", clientsRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+  console.log(`App is listening on ${PORT}`);
+});
